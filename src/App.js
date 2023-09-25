547bfd1a762f1f0201be26aa536e3c078047d516
foreman23 <foremanb@oregonstate.edu>
2023-09-24 19:03:32 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import { Form, Checkbox, Button, Input } from 'semantic-ui-react';
+import { useState } from 'react';
 
 function App() {
+
+  const submitRSVP = () => {
+    console.log(name, partyCount, email)
+  }
+
+  // State variables for form
+  const [name, setName] = useState('');
+  const [partyCount, setPartyCount] = useState(0);
+  const [email, setEmail] = useState('');
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body'>
+      {/* Header stuff */}
+      <div className='RSVPheader'>
+        <text>Are you attending?</text>
+        <text>RSVP</text>
+      </div>
+
+      {/* Form area */}
+      <div className='FormContainer'>
+        <Form className='RSVPform'>
+          <Form.Field>
+            <Input type='text' onChange={(event) => setName(event.target.value)} placeholder='Name' />
+          </Form.Field>
+          <Form.Field>
+            <Input type='number' onChange={(event) => setPartyCount(event.target.value)} placeholder='Number of persons' />
+          </Form.Field>
+          <Form.Field>
+            <Input type='email' onChange={(event) => setEmail(event.target.value)} placeholder='E-mail' />
+          </Form.Field>
+          <Button onClick={() => submitRSVP()} type='submit'>Confirm</Button>
+        </Form>
+      </div>
+
     </div>
+    
   );
 }
 
