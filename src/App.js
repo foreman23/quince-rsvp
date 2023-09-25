bd8c094fc460041ca9a8c365a78a384555aa0bbf
foreman23 <foremanb@oregonstate.edu>
2023-09-24 19:17:59 -0700

    radio inputs for rsvp form

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import { useState } from 'react';
 function App() {
 
   const submitRSVP = () => {
-    console.log(name, partyCount, email)
+    console.log(name, partyCount, email, isAttending)
   }
 
   // State variables for form
   const [name, setName] = useState('');
   const [partyCount, setPartyCount] = useState(0);
   const [email, setEmail] = useState('');
-
+  const [isAttending, setIsAttending] = useState(null);
 
   return (
     <div className='body'>
@@ -34,12 +34,36 @@ function App() {
           <Form.Field>
             <Input type='email' onChange={(event) => setEmail(event.target.value)} placeholder='E-mail' />
           </Form.Field>
+          <Form.Group>
+            <label>Will you be attending?</label>
+            <Form.Radio
+              label='Yes'
+              value='yes'
+              checked={isAttending === 'yes'}
+              onChange={() => setIsAttending('yes')}
+            >
+            </Form.Radio>
+            <Form.Radio
+              label='No'
+              value='no'
+              checked={isAttending === 'no'}
+              onChange={() => setIsAttending('no')}
+            >
+            </Form.Radio>
+            <Form.Radio
+              label='Maybe'
+              value='maybe'
+              checked={isAttending === 'maybe'}
+              onChange={() => setIsAttending('maybe')}
+            >
+            </Form.Radio>
+          </Form.Group>
           <Button onClick={() => submitRSVP()} type='submit'>Confirm</Button>
         </Form>
       </div>
 
     </div>
-    
+
   );
 }
 
